6d765c3bd67cae504371b28830e7183755698437
Gabriel Dison De Lima Reis <gabriel.disonreis@gmail.com>
2023-01-09 16:48:45 -0300

    feat: routes created and separated by resources

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
-import express, { response } from "express";
+import express from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json({ message: "hellow world" });
-});
+app.use(categoriesRoutes);
 
 app.listen(3333, () => console.log("server is running"));
