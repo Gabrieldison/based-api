ad4d869460467ab96a2c21e5e3753db9bd71e8f9
Gabriel Dison De Lima Reis <gabriel.disonreis@gmail.com>
2023-01-17 16:51:45 -0300

    feat: create stream anda read upload data

--- src/modules/cars/useCases/importCategories/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategories/ImportCategoryUseCase.ts
@@ -1,6 +1,46 @@
+import fs from "fs";
+import { parse as csvParse } from "csv-parse";
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
+
+interface IImportCategories {
+  name: string;
+  description: string;
+}
 class ImportCategoryUseCase {
-  execute(file: any) {
-    console.log(file);
+  constructor(private categoriesRepository: CategoriesRepository) {}
+  loadCategories(file: Express.Multer.File): Promise<IImportCategories[]> {
+    return new Promise((reoslve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCategories[] = [];
+
+      const parseFile = csvParse();
+
+      stream.pipe(parseFile);
+
+      parseFile
+        .on("data", async (line) => {
+          const [name, description] = line;
+
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on("end", () => {
+          reoslve(categories);
+        })
+        .on("error", (err) => {
+          reject(err);
+        });
+
+      return categories;
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+
+    console.log(categories);
   }
 }
 
