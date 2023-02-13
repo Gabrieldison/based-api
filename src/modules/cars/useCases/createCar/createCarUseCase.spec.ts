a224ce7aad84517ce7a2eefb9db3a206b8d498ec
Gabriel Dison De Lima Reis <gabriel.disonreis@gmail.com>
2023-02-13 10:51:42 -0300

    feat: creating part of the car creation API with tests using TDD

---
+++ src/modules/cars/useCases/createCar/createCarUseCase.spec.ts
@@ -0,0 +1,65 @@
+import { AppError } from "../../../../shared/errors/AppError";
+import { CarsRepositoryInMemory } from "../../repositories/in-memory/CarsRepositoryInMemory";
+import { CreateCarUseCase } from "./createCarUseCase";
+
+let createCarUseCase: CreateCarUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+
+describe("Create Car", () => {
+  beforeEach(() => {
+    carsRepositoryInMemory = new CarsRepositoryInMemory();
+    createCarUseCase = new CreateCarUseCase(carsRepositoryInMemory);
+  });
+
+  it("should be able to create a new car", async () => {
+    const car = await createCarUseCase.execute({
+      brand: "Name Car",
+      category_id: "Description Car",
+      daily_rate: 100,
+      description: "ABC-1234",
+      fine_amount: 60,
+      license_plate: "Brand",
+      name: "category",
+    });
+
+    expect(car).toHaveProperty("id");
+  });
+
+  it("should not be able to create a car with exists license plate", async () => {
+    expect(async () => {
+      await createCarUseCase.execute({
+        brand: "Car1",
+        category_id: "Description Car",
+        daily_rate: 100,
+        description: "ABC-1234",
+        fine_amount: 60,
+        license_plate: "Brand",
+        name: "category",
+      });
+
+      await createCarUseCase.execute({
+        brand: "Car2",
+        category_id: "Description Car",
+        daily_rate: 100,
+        description: "ABC-1234",
+        fine_amount: 60,
+        license_plate: "Brand",
+        name: "category",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should not be able to create a car with available true by default", async () => {
+    const car = await createCarUseCase.execute({
+      brand: "Car Available",
+      category_id: "Description Car",
+      daily_rate: 100,
+      description: "ABC-1234",
+      fine_amount: 60,
+      license_plate: "Brand",
+      name: "category",
+    });
+
+    expect(car.available).toBe(true);
+  });
+});
---
+++ src/modules/cars/useCases/createCar/createCarUseCase.ts
@@ -0,0 +1,44 @@
+import { inject, injectable } from "tsyringe";
+import { AppError } from "../../../../shared/errors/AppError";
+import { ICreateCarDTO } from "../../dtos/ICreateCarDTO";
+import { Car } from "../../infra/typeorm/entities/Car";
+import { ICarsRepository } from "../../repositories/ICarsRepository";
+
+//@injectable()
+class CreateCarUseCase {
+  constructor(
+    //   @inject("CarsRepository")
+    private carsRepository: ICarsRepository
+  ) {}
+  async execute({
+    brand,
+    category_id,
+    daily_rate,
+    description,
+    fine_amount,
+    license_plate,
+    name,
+  }: ICreateCarDTO): Promise<Car> {
+    const carAlreadyExists = await this.carsRepository.findByLicensePlate(
+      license_plate
+    );
+
+    if (carAlreadyExists) {
+      throw new AppError("Car already exists");
+    }
+
+    const car = await this.carsRepository.create({
+      name,
+      description,
+      daily_rate,
+      license_plate,
+      fine_amount,
+      brand,
+      category_id,
+    });
+
+    return car;
+  }
+}
+
+export { CreateCarUseCase };
