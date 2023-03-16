c3bc270752ce8301bd62cf6e3c1a99f9d80b8cb2
Gabriel Dison De Lima Reis <gabriel.disonreis@gmail.com>
2023-03-16 19:01:23 -0300

    feat: create provider of send email

---
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -0,0 +1,50 @@
+import { inject, injectable } from "tsyringe";
+import { IDateProvider } from "../../../../shared/container/providers/DateProvider/IDateProvider";
+import { AppError } from "../../../../shared/errors/AppError";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { IUserTokensRepository } from "../../repositories/IUserTokensRepository";
+import { v4 as uuidV4 } from "uuid";
+import { IMailProvider } from "../../../../shared/container/providers/MailProvider/IMailProvider";
+
+@injectable()
+class SendForgotPasswordMailUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository,
+
+    @inject("UserTokensRepository")
+    private usersTokensRepository: IUserTokensRepository,
+
+    @inject("DayjsDateProvider")
+    private dateProvider: IDateProvider,
+
+    @inject("EtherealMailProvider")
+    private mailProvider: IMailProvider
+  ) {}
+
+  async execute(email: string): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError("User does not exists!");
+    }
+
+    const token = uuidV4();
+
+    const expires_date = this.dateProvider.addHours(3);
+
+    await this.usersTokensRepository.create({
+      refresh_token: token,
+      user_id: user.id,
+      expires_date,
+    });
+
+    await this.mailProvider.sendMail(
+      email,
+      "Recuperação de senha",
+      `O link para o reset é ${token}`
+    );
+  }
+}
+
+export { SendForgotPasswordMailUseCase };
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { authenticateRoutes } from "./authenticate.routes";
 import { carRoutes } from "./cars.routes";
 import { categoriesRoutes } from "./categories.routes";
+import { passwordRoutes } from "./passwords.routes";
 import { rentalRoutes } from "./rental.routes";
 import { specificationsRoutes } from "./specification.routes";
 import { userRoutes } from "./users.routes";
@@ -13,6 +14,7 @@ router.use("/specifications", specificationsRoutes);
 router.use("/users", userRoutes);
 router.use("/cars", carRoutes);
 router.use("/rentals", rentalRoutes);
+router.use("/password", passwordRoutes);
 router.use(authenticateRoutes);
 
 export { router };
