91fa8ec586c32623fc9fba6ddac6e0a2c6fad2e5
Gabriel Dison De Lima Reis <gabriel.disonreis@gmail.com>
2023-03-20 15:31:02 -0300

    feat: create provider upload of localstorage

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -17,7 +17,7 @@ const createCarspecificationController =
   new CreateCarsSpecificationController();
 const uploadCarImagesController = new UploadCarImageController();
 
-const upload = multer(uploadConfig.upload("./tmp/cars"));
+const upload = multer(uploadConfig);
 
 carRoutes.post(
   "/",
