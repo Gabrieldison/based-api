50f1fe8daac550b5d4eed6f62a4dc2fa7e0f4e3f
Gabriel Dison De Lima Reis <gabriel.disonreis@gmail.com>
2023-03-21 15:39:24 -0300

    feat: doing upload avatar image with  s3

--- src/modules/cars/useCases/uploadCarImages/UploadCarImageUseCase.ts
+++ src/modules/cars/useCases/uploadCarImages/UploadCarImageUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe";
+import { IStorageProvider } from "../../../../shared/container/providers/StorageProvider/IStorage";
 import { ICarsImageRepository } from "../../repositories/ICarsImageRepository";
 
 interface IRequest {
@@ -10,12 +11,16 @@ interface IRequest {
 class UploadCarsImageUseCase {
   constructor(
     @inject("CarsImagesRepository")
-    private carsImagesRepository: ICarsImageRepository
+    private carsImagesRepository: ICarsImageRepository,
+
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider
   ) {}
 
   async execute({ car_id, name_image }: IRequest): Promise<void> {
     name_image.map(async (image) => {
       await this.carsImagesRepository.create(car_id, image);
+      await this.storageProvider.save(image, "cars");
     });
   }
 }
